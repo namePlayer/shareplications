7a8965157f5b8e1085b5cdcc902c9d2d5a7c00d7
namePlayer <robin@npcore.net>
2021-08-20 22:57:03 +0200

    Add even more Base Files

--- routes.php
+++ routes.php
@@ -1,1 +1,11 @@
 <?php
+
+$requestedUrl = $_SERVER['REQUEST_URI'];
+$requestedPath = str_replace('/index.php','', $_SERVER['SCRIPT_NAME']);
+$requestedUrl = str_replace($requestedPath, '', $requestedUrl);
+
+router('/', function() use ($requestedPath) {
+
+}, 'POST|GET');
+
+echo router($requestedUrl);
--- script/router.php
+++ script/router.php
@@ -1,1 +1,39 @@
 <?php
+declare(strict_types=1);
+
+function router($path = null, $action = null, $methods = 'POST|GET',bool $directRequestDisabled = false) {
+    static $routes = [];
+
+    if(!$path){
+        return $routes;
+    }
+    if(strpos($path, '..') !== false){
+        return NULL;
+    }
+
+    if ($action) {
+        return $routes['(' . $methods . ')_' . $path] = [$action,$directRequestDisabled];
+    }
+    $originalPath = str_replace('?'.$_SERVER['QUERY_STRING'], '', $path);
+    $path = $_SERVER['REQUEST_METHOD'].'_'.$originalPath;
+    foreach ($routes as $route => $data) {
+        list($action,$currentDirectRequestIsDisabled) = $data;
+        $regEx = "~^$route/?$~i";
+
+        $matches = [];
+        if (!preg_match($regEx, $path, $matches)) {
+            continue;
+        }
+        if (!is_callable($action)) {
+            return [$path, 'Route not found'];
+        }
+        if($currentDirectRequestIsDisabled && $_SERVER['REQUEST_URI'] && $_SERVER['REQUEST_URI'] === $originalPath){
+            return [$path, 'Route not found'];
+        }
+        array_shift($matches);
+        array_shift($matches);
+        $response = $action(...$matches);
+        return $response;
+    }
+    return [$path, 'Route not found'];
+}
